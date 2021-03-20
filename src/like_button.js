a19a31fdaa89eca1498b24d52e2c586104c34a17
Ryder <ryderarenfranklin@gmail.com>
2021-03-20 20:03:36 +0000

    various edits to get like button to work (almost)

---
+++ src/like_button.js
@@ -0,0 +1,23 @@
+//
+'use strict';
+
+class likeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {liked: false};
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return (
+            <button onClick={() => this.setState({ liked: true})}>
+            Like 
+            </button>
+        );
+    }
+}
+let domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(<likeButton />, domContainer);
