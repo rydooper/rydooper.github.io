1b64208d0fbd1f17c1368648f7cc3a3d1c7ebd1b
Ryder <ryderarenfranklin@gmail.com>
2022-04-24 15:24:17 +0100

    formatting changes

--- website-js.js
+++ website-js.js
@@ -2,7 +2,7 @@ function openTab(tabName) {
     var i, x;
     x = document.getElementsByClassName("expandable-projects");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+        x[i].style.display = "none";
     }
     document.getElementById(tabName).style.display = "block";
-  } 
+} 
