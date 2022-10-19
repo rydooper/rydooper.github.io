0afee011428ca2033ea0626f8c0b7c7a86d71610
Ryder <ryderarenfranklin@gmail.com>
2022-10-19 10:13:42 +0100

    various updates to fix styling

---
+++ header.js
@@ -0,0 +1,35 @@
+document.write('<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">\
+<div class="container-fluid">\
+    <a class="navbar-brand" href="index.html">\
+        <img src="../assets/images/homeIcon.png" alt="home image" width="30" height="30"\
+            class="d-inline-block align-text-top">\
+        Home\
+    </a>\
+    <a class="navbar-brand" href="projects.html">\
+        <img src="../assets/images/folder.svg" alt="folder image" width="30" height="30"\
+            class="d-inline-block align-text-top">\
+        Projects\
+    </a>\
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"\
+        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"\
+        aria-label="Toggle navigation">\
+        <span class="navbar-toggler-icon"></span>\
+    </button>\
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">\
+            <li class="nav-item dropdown">\
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"\
+                    data-bs-toggle="dropdown" aria-expanded="false">\
+                    Contact Links\
+                </a>\
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">\
+                    <li><a href="https://github.com/rydooper"><img class="dropdown-item"\
+                                src="assets/images/github.svg"><p>Github</p></a></li>\
+                    <li><a href="https://www.linkedin.com/in/ryderfranklin2000"><img class="dropdown-item"\
+                                src="../assets/images/linkedin.svg"><p>LinkedIn</p></a></li>\
+                </ul>\
+            </li>\
+        </ul>\
+    </div>\
+</div>\
+</nav>');
