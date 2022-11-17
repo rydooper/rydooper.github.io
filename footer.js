ad95bec38f81244edde620fec7ec47a0b5fb418b
rydooper <ryderarenfranklin@gmail.com>
2022-11-17 17:46:03 +0000

    updated footer

--- footer.js
+++ footer.js
@@ -1,1 +1,3 @@
-document.write('<footer class=>');
+document.write('<div class="footer"> Be sure to check out the best <a \
+href="https://music.youtube.com/playlist?list=PLXY_0YgPbMrPAcJdoN2yd_SNo9OsiIsNI"> lofi playlist </a> \
+around! </div>');
